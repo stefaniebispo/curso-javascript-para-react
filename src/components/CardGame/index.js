1c8a7b7878a532d06f327b6d25750f2e665b8fde
Stefanie Bispo <bispostefanie@outlook.com>
2023-01-24 19:32:21 -0300

    :tada: Create CardGame Component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+//JSX é similar
+function CardGame(){
+  return `
+    <article class='card-game'>
+      <img src='images/alura-pixel.png' alt='logo da alura'>
+    </article>
+  `;
+}
+
+export default CardGame;
